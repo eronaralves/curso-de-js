0eca5487d5301fc8286906932287f555d8c2433f
eronaralves <89744967+eronaralves@users.noreply.github.com>
2022-02-02 06:13:55 -0300

    exercicios de estrutura de dados

---
+++ exercicios/exercicio-estrutura-de-dados/script.js
@@ -0,0 +1,11 @@
+let nome = "Eronar"
+let idade = 15
+let ano = 2022
+
+console.log(idade - ano)
+console.log(idade + ano)
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
