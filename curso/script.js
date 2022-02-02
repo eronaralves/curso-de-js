b4b97b2868f0c42b30b34f8ac37d4943e4589681
eronaralves <89744967+eronaralves@users.noreply.github.com>
2022-02-01 23:42:35 -0300

    aprendendo funcoes

---
+++ curso/script.js
@@ -0,0 +1,13 @@
+
+/**
+ * Funcao que mostra no console
+ * tres frases movitacionais
+ */
+
+function textos() {
+    console.log("fsjdf")
+    console.log("fskfdm")
+    console.log("fdskmf")
+}
+
+textos()
