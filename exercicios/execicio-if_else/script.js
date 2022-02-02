e173307935de8d1de4724db5951ab0c330802056
eronaralves <89744967+eronaralves@users.noreply.github.com>
2022-02-02 15:15:03 -0300

    exercicio de if e else

---
+++ exercicios/execicio-if_else/script.js
@@ -0,0 +1,43 @@
+
+let humano = true
+let idade = 15
+
+if(idade > 18) {
+    console.log("Voce é maior de idade")
+} else {
+    console.log("Voce é menor de idade")
+}
+
+if(idade > 18 && humano == true) {
+    console.log("Voce pode tirar carteira")
+} else {
+    console.log("Voce nao tem idade suficiente")
+}
+
+let mes = "Julho"
+
+if(mes === "Janeiro" || mes === "Dezembro") {
+    console.log("Esse é seu mês")
+} else {
+    console.log("Esse não é o seu mes")
+}
+
+let nome = "aronar"
+
+if(nome[0] === "R"){
+    console.log("Seu nome comeca com R")
+}else {
+    console.log("Seu nome não comeca com R")
+}
+
+let sobrenome = "Alves"
+
+if(/*Nao conseguir fazer essa parte*/|| nome[0] == "E") {
+    console.log("Voce tem a descricao certa")
+}else {
+    console.log("Voce nao tem a descricao certa")
+}
+
+
+
+
