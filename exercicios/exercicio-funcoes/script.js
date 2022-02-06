2eea2c6b1b8db107fddbd96ae5b23426c668993e
eronaralves <89744967+eronaralves@users.noreply.github.com>
2022-02-06 17:51:43 -0300

    exercicio de funcao

---
+++ exercicios/exercicio-funcoes/script.js
@@ -0,0 +1,35 @@
+//
+function alerta() {
+    console.log("Se beber nao dirija")
+}
+
+alerta()
+
+//
+function meuNome(nome) {
+    console.log(nome)
+}
+
+meuNome("Eronar")
+
+//
+function dados(nome, numero, estilo) {
+    console.log(nome, numero, estilo)
+}
+
+dados("Eronar", 15, "rap")
+
+//
+function gostos(filme, musica) {
+    console.log(`meu filme favorito é ${filme} e uma das minhas musicas favoritas é ${musica}`)
+}
+
+gostos("Gigante de aço", "Ninguem me ensinou")
+
+//
+function conta(n) {
+    return n * 3
+}
+
+conta(7)
+console.log(conta(7))
