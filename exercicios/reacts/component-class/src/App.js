5d33b6b836982d81bee75e55b4fdd211f0c300f4
eronaralves <89744967+eronaralves@users.noreply.github.com>
2022-02-07 07:09:39 -0300

    Component class

---
+++ exercicios/reacts/component-class/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+class teste extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Ola, tudo bem ?</h1>
+      </div>
+    )
+  }
+}
+
+export default teste
